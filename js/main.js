7ead7cbdb85b3ea05a83bd69dfcb8d951f8abeb6
Arturo Simon <artysimon@gmail.com>
2020-10-26 19:59:34 -0400

    Switch default theme to dark

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const body = document.getElementsByTagName("body")[0];
 
 // Change theme when checkbox is checked
 function changeTheme(element) {
-    let = darkThemeClassName = "dark-magic";
+    let = whiteThemeClassName = "white-magic";
 
     if (element.checked) {
-        body.classList.add(darkThemeClassName);
+        body.classList.add(whiteThemeClassName);
     } else {
-        body.classList.remove(darkThemeClassName);
+        body.classList.remove(whiteThemeClassName);
     }
 }
 
