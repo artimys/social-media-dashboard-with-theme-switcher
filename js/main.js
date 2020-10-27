3ff9604927892811a1c5ba44e1f0d546c1c88b9a
Arturo Simon <artysimon@gmail.com>
2020-10-26 21:51:08 -0400

    Attempt at keyframes and locatorStorage

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
 const checkbox = document.getElementById("checkbox");
 const body = document.getElementsByTagName("body")[0];
+const notification = document.getElementById("notification");
 
-
-// Change theme when checkbox is checked
-function changeTheme(element) {
-    let = whiteThemeClassName = "white-magic";
-
-    if (element.checked) {
-        body.classList.add(whiteThemeClassName);
-    } else {
-        body.classList.remove(whiteThemeClassName);
-    }
+const themes = {
+    dark: "dark-magic",
+    light: "light-magic"
+}
+const savedTheme = localStorage.getItem("theme");
+function saveTheme(theme) {
+    localStorage.setItem("theme", theme);
 }
 
+
 // Click event for checkbox
+// Change theme when checkbox is checked
 checkbox.addEventListener("click", function(e) {
-    changeTheme(this);
+    if (this.checked) {
+        body.classList.add(themes.light);
+        saveTheme(themes.light);
+    } else {
+        body.classList.remove(themes.light);
+        saveTheme(themes.dark);
+    }
+
+    // Display notification that theme was saved
+    notification.classList.add("notification--animate");
 });
 
 
-// Change team on page load
-changeTheme(checkbox);
+
+// End animation event to remove class that animates
+notification.addEventListener("animationend", function() {
+    notification.classList.remove("notification--animate");
+});
+
+
+
+// Use localStorage to get user theme selection
+if (savedTheme !==null && savedTheme === themes.light) {
+    // Check the box
+    checkbox.checked = true;
+    // Add the white theme class
+    body.classList.add(themes.light);
+}
+// otherwise default to dark theme
